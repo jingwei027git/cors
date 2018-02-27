127155b1d31255fcdd953bd5c190f5f8db54faed
david <jingwei027@gmail.com>
2018-02-27 16:33:32 +0800

    .

--- src/main/java/tw/com/softpower/demo/cors/web/MainAction.java
+++ src/main/java/tw/com/softpower/demo/cors/web/MainAction.java
@@ -2,15 +2,15 @@ package tw.com.softpower.demo.cors.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tw.com.softpower.demo.cors.dto.Client;
 
 import javax.servlet.http.HttpServletResponse;
 
-// @CrossOrigin(origins = "https://booking.attractionsuite.com", maxAge = 3600)
-@RequestMapping("/client")
-@RestController
-public class DemoController {
+@RequestMapping("/main")
+@Controller
+public class MainAction {
 
     @ModelAttribute
     public void setVaryResponseHeader(HttpServletResponse response) {
@@ -18,12 +18,8 @@ public class DemoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Client> read(@PathVariable Long id) {
-        final Client client = new Client();
-        client.setName("eddie");
-        client.setAge(39);
-
-        return new ResponseEntity<>(client, HttpStatus.OK);
+    public String read(@PathVariable Long id) {
+        return "index";
     }
 
 }
